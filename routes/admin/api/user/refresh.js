e05c8f0bbc31403c892e1d9a1c111ba39db09da6
elenahao <elena_haoo@163.com>
2015-09-25 10:07:42 +0800

    add log

--- routes/admin/api/user/refresh.js
+++ routes/admin/api/user/refresh.js
@@ -21,40 +21,40 @@ app.get('/admin/api/refresh/user', function(req, res) {
             console.log(res.access_token);
             ACCESS_TOKEN = res.access_token;
             //var next_openid = 'o0aT-dzYotN0c1QJeejYOGStmKFQ';
-            //var next_openid = 'o0aT-d-2Ml_edNGuWquQQki_Xpgk';
-            //getUser(ACCESS_TOKEN, next_openid);
-            request({
-                url: 'https://api.weixin.qq.com/cgi-bin/user/get?access_token='+ACCESS_TOKEN,
-                method: 'GET'
-            }, function(err, res, body) {
-                if(err) console.log(err);
-                console.log('======'+body);
-                if (res.statusCode === 200) {
-                    console.log('success');
-                    //存入redis
-                    var _body = JSON.parse(body);
-                    var total = _body.total;
-                    var count = _body.count;
-                    var data = _body.data;
-                    var openids = data.openid;
-                    var next_openid = _body.next_openid;
-                    for(var i = 0; i< openids.length; i++){
-                        var openid = openids[i];
-                        var options = {
-                            openid : openid
-                        }
-                        redis.hmset('user:'+openid, options)
-                            .then(function resolve(res) {
-                                console.log('is set ok:', res);
-                            }, function reject(err) {
-                                dfd.reject(err);
-                            })
-                    }
-                    if(total != count){
-                        getUser(ACCESS_TOKEN, next_openid);
-                    }
-                }
-            });
+            var next_openid = 'o0aT-dxYVtb7ZeNbcFdQ4B2D1lsU';
+            getUser(ACCESS_TOKEN, next_openid);
+            //request({
+            //    url: 'https://api.weixin.qq.com/cgi-bin/user/get?access_token='+ACCESS_TOKEN,
+            //    method: 'GET'
+            //}, function(err, res, body) {
+            //    if(err) console.log(err);
+            //    console.log('======'+body);
+            //    if (res.statusCode === 200) {
+            //        console.log('success');
+            //        //存入redis
+            //        var _body = JSON.parse(body);
+            //        var total = _body.total;
+            //        var count = _body.count;
+            //        var data = _body.data;
+            //        var openids = data.openid;
+            //        var next_openid = _body.next_openid;
+            //        for(var i = 0; i< openids.length; i++){
+            //            var openid = openids[i];
+            //            var options = {
+            //                openid : openid
+            //            }
+            //            redis.hmset('user:'+openid, options)
+            //                .then(function resolve(res) {
+            //                    console.log('is set ok:', res);
+            //                }, function reject(err) {
+            //                    dfd.reject(err);
+            //                })
+            //        }
+            //        if(total != count){
+            //            getUser(ACCESS_TOKEN, next_openid);
+            //        }
+            //    }
+            //});
         }
     },function reject(err){
         res.status(400).send(JSON.stringify({
@@ -62,7 +62,7 @@ app.get('/admin/api/refresh/user', function(req, res) {
             msg: errors
         }));
     })
-    res.redirect('/admin/user');
+    //res.redirect('/admin/user');
 });
 
 var getUser = function(ACCESS_TOKEN, next_openid) {
@@ -88,13 +88,14 @@ var getUser = function(ACCESS_TOKEN, next_openid) {
             }
             redis.hmset('user:'+openid, options)
                 .then(function resolve(res) {
-                    console.log('is set ok:', res);
+                    //console.log('is set ok:', res);
                 }, function reject(err) {
                     dfd.reject(err);
                 })
         }
         if(count != 10000){
             //结束,last time
+            console.log('last time......................');
             request({
                 url: 'https://api.weixin.qq.com/cgi-bin/user/get?access_token='+ACCESS_TOKEN+'&next_openid='+next_openid,
                 method: 'GET'
